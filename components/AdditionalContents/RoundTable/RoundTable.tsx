1e85b3ff5cc27f94f6114c81b9603443ce42d621
Taron <taron.aleksanyan.20@gmail.com>
2022-12-25 20:28:32 +0400

    feat: round table

--- components/AdditionalContents/RoundTable/RoundTable.tsx
+++ components/AdditionalContents/RoundTable/RoundTable.tsx
@@ -1,8 +1,44 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
+import strtr from "../../../utils/strTr";
 import { data } from "./data";
+import styles from "./RoundTable.module.css";
 
-export default function RoundTable() {
+export default function RoundTable({ language = "en" }) {
   const [tableNumber, setTableNumber] = useState(0);
-  return <>
-  </>;
+  const title = useMemo(() => {
+    return strtr(data.messages?.[language].title, [
+      data.tables[tableNumber].players1,
+      data.tables[tableNumber].players2,
+    ]);
+  }, [tableNumber, language]);
+
+  const handleTableChange = (e) =>
+    setTableNumber(e.target.getAttribute("data-index"));
+
+  return (
+    <div className={styles.round_table_wrapper}>
+      <h2>{title}</h2>
+      <div className={styles.table_buttons_wrapper}>
+        {data.tables.map(({ players1, players2 }, index) => {
+          return (
+            <button onClick={handleTableChange} data-index={index}>
+              {players1} - {players2}
+            </button>
+          );
+        })}
+      </div>
+      <table className={styles.table}>
+        {data.tables[tableNumber].rows.map((row, index) => {
+          return (
+            <tr>
+              <td>Tour {index + 1}</td>
+              {row.map((field) => {
+                return <td>{field}</td>;
+              })}
+            </tr>
+          );
+        })}
+      </table>
+    </div>
+  );
 }
---
+++ utils/strTr.ts
@@ -0,0 +1,9 @@
+export default function strtr(str: string, replacement: string[]): string {
+  let result = str;
+  const matches = str.match(/\$\d+/g);
+  matches?.forEach((match, index) => {
+    result = result.replaceAll(match, replacement[index]);
+  });
+
+  return result;
+}
