4a1304e4f737188025f252aa9813c7e83088873f
Taron <taron.aleksanyan.20@gmail.com>
2022-12-28 17:00:08 +0400

    fix: minor changes

--- hoc/withClientSideComponent.tsx
+++ hoc/withClientSideComponent.tsx
@@ -1,7 +1,8 @@
 import React, { Component, useEffect, useState } from "react";
 
 export default function withClientSideComponent(
-  Component: React.FC<Component>
+  Component: React.FC<Component>,
+  Fallback?: React.FC<Component>
 ) {
   return function Render(props: any) {
     const [isReady, setIsReady] = useState(false);
@@ -11,7 +12,7 @@ export default function withClientSideComponent(
     }, []);
 
     if (!isReady) {
-      return null;
+      return Fallback || <div />;
     }
     return <Component {...props} />;
   };
