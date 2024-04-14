85be2a081166824a04034ab283a720ab81990848
Artyom Sukiasyan <artiom.suqiasyan1401@gmail.com>
2024-04-14 23:42:28 +0400

    feat: add about us initial version

---
+++ app/about/data.ts
@@ -0,0 +1,23 @@
+import ArtyomPhoto from "../../public/images/team/artyom.webp";
+import TaronPhoto from "../../public/images/team/taron.webp";
+
+export const data = {
+  artyom: {
+    name: "Artyom Sukiasyan",
+    img: ArtyomPhoto,
+    bio: "Artyom bio",
+    links: {
+      github: "https://github.com/ArtyomSukiasyan/",
+      linkedin: "https://www.linkedin.com/in/artyomsukiasyan/",
+    },
+  },
+  taron: {
+    name: "Taron Aleksanyan",
+    img: TaronPhoto,
+    bio: "Taron bio",
+    links: {
+      facebook: "",
+      gmail: "",
+    },
+  },
+};
---
+++ app/about/page.tsx
@@ -0,0 +1,52 @@
+import Image from "next/image";
+import styles from "../../styles/about.module.css";
+import { data } from "./data";
+
+export default function About() {
+  return (
+    <div className={styles.container}>
+      <p>
+        The Chess Arbiter program is NOT an official FIDE program, it was
+        created by the initiative of two chess players. When editing the
+        material, materials from the official FIDE website, websites of chess
+        federations of different countries, as well as from other chess websites
+        were used and translated. Below is our team.
+      </p>
+      <h2 className={styles.h2}>Our team</h2>
+      <div className={styles.card_container}>
+        <Card person="artyom" />
+        <Card person="taron" />
+      </div>
+    </div>
+  );
+}
+
+function Card({ person }: { person: "artyom" | "taron" }) {
+  const { name, bio, img, links } = data[person];
+
+  return (
+    <div>
+      <h3 className={styles.h3}>{name}</h3>
+      <div className={styles.card}>
+        <div className={styles.imgBox}>
+          <Image src={img} alt={name} priority />
+        </div>
+        <div className={styles.details}>
+          <h2>{name}</h2>
+          <p>{bio}</p>
+        </div>
+      </div>
+      <div>
+        {Object.entries(links).map(([key, value]) => {
+          return (
+            <div>
+              <a href={value.toString()} target="_blank" rel="no-referer">
+                {key}
+              </a>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
