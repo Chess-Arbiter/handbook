a165e5ec3f74f229551d6c828947c2aa6ab30635
Artyom Sukiasyan <artiom.suqiasyan1401@gmail.com>
2024-04-17 21:09:30 +0400

    feat: optimize LanguageSelect function

--- components/LanguagesSelect/LanguagesSelect.tsx
+++ components/LanguagesSelect/LanguagesSelect.tsx
@@ -1,4 +1,3 @@
-import { useMemo } from "react";
 import Link from "next/link";
 import { ELANGUAGES } from "../../models/languages";
 import getLink from "../../utils/getLink";
@@ -6,21 +5,16 @@ import styles from "./LanguagesSelect.module.css";
 import Icon from "../Icon/Icon";
 import useMobile from "../../hooks/useMobile";
 
-export default function LanguagesSelect({
-  lang,
-  asPath,
-}: {
+interface ILangSelect {
   lang: ELANGUAGES;
   asPath: string;
-}) {
-  const languages = useMemo((): ELANGUAGES[] => {
-    const ALL_LANGUAGES = [ELANGUAGES.EN, ELANGUAGES.RU, ELANGUAGES.HY];
-    const currentLanguage: ELANGUAGES = lang || ALL_LANGUAGES[0];
-    ALL_LANGUAGES.splice(ALL_LANGUAGES.indexOf(currentLanguage), 1);
-    ALL_LANGUAGES.unshift(currentLanguage);
+}
 
-    return ALL_LANGUAGES;
-  }, [lang]);
+export default function LanguagesSelect({ lang, asPath }: ILangSelect) {
+  const ALL_LANGUAGES = [ELANGUAGES.EN, ELANGUAGES.RU, ELANGUAGES.HY];
+
+  const languages = [lang, ...ALL_LANGUAGES.filter((l) => l !== lang)];
+
   const isMobile = useMobile();
 
   const linkIdx = 0;
