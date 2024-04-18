cd580b20f77482422ffa395ee472f5b77b14fbbe
Artyom Sukiasyan <artiom.suqiasyan1401@gmail.com>
2024-04-18 11:00:01 +0400

    fix: add title to 404 page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,3 +1,9 @@
 import NotFound from "../components/404/404";
+import { siteTile } from "../constants/titles";
+
+export const metadata = {
+  title: `Page not found | ${siteTile}`,
+  description: `Page not found on site ${siteTile}`,
+};
 
 export default NotFound;
