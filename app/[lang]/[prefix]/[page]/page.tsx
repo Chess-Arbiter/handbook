8a979b1f01191b2b53d277bc56c49439d47e1fa7
Artyom Sukiasyan <artiom.suqiasyan1401@gmail.com>
2024-10-07 23:42:34 +0400

    feat: cleanup main files

--- app/[lang]/[prefix]/[page]/page.tsx
+++ app/[lang]/[prefix]/[page]/page.tsx
@@ -1,6 +1,5 @@
 import MainLayout from "../../../../layouts/MainLayout";
 import AdditionalContents from "../../../../components/AdditionalContents/AdditionalContents";
-import getPage from "../../../../utils/getPage";
 import { siteTile } from "../../../../constants/titles";
 import { IPage, IPageMetadata } from "../../../../models/page";
 import { ELANGUAGES } from "../../../../models/languages";
@@ -10,7 +9,6 @@ import fs from "fs";
 import path from "path";
 import Markdown from "react-markdown";
 import remarkGfm from "remark-gfm";
-import TurndownService from "turndown";
 
 interface IPageParams {
   lang: ELANGUAGES;
@@ -28,7 +26,7 @@ export async function generateMetadata({ params }: { params: IPageParams }) {
 }
 
 export default async function PageContent({ params }: { params: IPageParams }) {
-  const page = await getDataM({ params });
+  const page = await getData({ params });
 
   return (
     <>
@@ -40,9 +38,7 @@ export default async function PageContent({ params }: { params: IPageParams }) {
   );
 }
 
-async function getDataM({ params }: { params: IPageParams }) {
-  // console.log({ params });
-
+async function getData({ params }: { params: IPageParams }) {
   const postsDirectory = path.join(
     process.cwd(),
     "public",
@@ -58,7 +54,6 @@ async function getDataM({ params }: { params: IPageParams }) {
   const metaDataJson = fs.readFileSync(metadataPath, "utf8");
 
   const postData: IPageMetadata = JSON.parse(metaDataJson);
-  // console.log({ postData });
 
   const page: IPage = {
     content: fileContents,
@@ -66,65 +61,4 @@ async function getDataM({ params }: { params: IPageParams }) {
   };
 
   return page;
-}
-
-async function getData({ params }: { params: IPageParams }) {
-  // console.log({ params });
-
-  const pageDoc = await getPage(params.page);
-  const page: IPage = {
-    title: pageDoc?.[`title_${params.lang}`] || "",
-    description: pageDoc?.[`description_${params.lang}`] || "Chess rules",
-    content: pageDoc?.[`content_${params.lang}`] || "",
-    parent: pageDoc.parent || "",
-    slug: pageDoc?.slug || "",
-  };
-  // console.log({ page });
-
-  const postsDirectory = path.join(
-    process.cwd(),
-    "public",
-    "content",
-    `${params.lang}`,
-    `${params.prefix}`,
-    `${params.page}`
-  );
-  const turndownService = new TurndownService();
-  const markdownContent = turndownService.turndown(page.content);
-
-  const isCreated = createFolderIfNotExists(postsDirectory);
-  if (isCreated) {
-    createMarkdownFile(postsDirectory, "content", markdownContent);
-    createJsonFile(postsDirectory, "metadata", {
-      description: page.description,
-      parent: page.parent,
-      slug: page.slug,
-      title: page.title,
-    });
-  }
-
-  return page;
-}
-
-function createFolderIfNotExists(folderPath: string) {
-  if (!fs.existsSync(folderPath)) {
-    fs.mkdirSync(folderPath, { recursive: true });
-    return true;
-  }
-  return false;
-}
-
-// Function to create a markdown file
-function createMarkdownFile(folder: string, fileName: string, content: string) {
-  const filePath = path.join(folder, `${fileName}.md`);
-  console.log({ filePath });
-  fs.writeFileSync(filePath, content, "utf-8");
-  console.log(`${fileName}.md created successfully!`);
-}
-
-// Function to create a JSON file
-function createJsonFile(folder: string, fileName: string, jsonObject: any) {
-  const filePath = path.join(folder, `${fileName}.json`);
-  fs.writeFileSync(filePath, JSON.stringify(jsonObject, null, 2), "utf-8");
-  console.log(`${fileName}.json created successfully!`);
 }
